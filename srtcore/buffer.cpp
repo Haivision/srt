93d0977771f669a3a0a8f399a3f643df9e86cc08
Mikołaj Małecki <mmalecki@haivision.com>
2017-10-05 16:58:21 +0200

    Changed extracting packet to only sequence number

--- srtcore/buffer.cpp
+++ srtcore/buffer.cpp
@@ -931,7 +931,7 @@ void CRcvBuffer::skipData(int len)
       m_iMaxPos = 0;
 }
 
-bool CRcvBuffer::getRcvFirstMsg(ref_t<uint64_t> tsbpdtime, ref_t<bool> passack, ref_t<int32_t> skipseqno, CPacket** pppkt)
+bool CRcvBuffer::getRcvFirstMsg(ref_t<uint64_t> tsbpdtime, ref_t<bool> passack, ref_t<int32_t> skipseqno, ref_t<int32_t> r_curpktseq)
 {
     skipseqno = -1;
     passack = false;
@@ -944,7 +944,7 @@ bool CRcvBuffer::getRcvFirstMsg(ref_t<uint64_t> tsbpdtime, ref_t<bool> passack, 
     // - @return: whether the reported packet is ready to play
 
     /* Check the acknowledged packets */
-    if (getRcvReadyMsg(tsbpdtime, pppkt))
+    if (getRcvReadyMsg(tsbpdtime, r_curpktseq))
     {
         return true;
     }
@@ -1041,7 +1041,7 @@ bool CRcvBuffer::getRcvFirstMsg(ref_t<uint64_t> tsbpdtime, ref_t<bool> passack, 
     return false;
 }
 
-bool CRcvBuffer::getRcvReadyMsg(ref_t<uint64_t> tsbpdtime, CPacket** pppkt)
+bool CRcvBuffer::getRcvReadyMsg(ref_t<uint64_t> tsbpdtime, ref_t<int32_t> curpktseq)
 {
    tsbpdtime = 0;
    int rmpkts = 0; 
@@ -1058,8 +1058,7 @@ bool CRcvBuffer::getRcvReadyMsg(ref_t<uint64_t> tsbpdtime, CPacket** pppkt)
          continue;
       }
 
-      if ( pppkt )
-          *pppkt = &m_pUnit[i]->m_Packet;
+      curpktseq = m_pUnit[i]->m_Packet.getSeqNo();
 
       if (m_pUnit[i]->m_iFlag != CUnit::GOOD)
       {
@@ -1106,7 +1105,7 @@ bool CRcvBuffer::getRcvReadyMsg(ref_t<uint64_t> tsbpdtime, CPacket** pppkt)
 * used in the code (core.cpp) is expensive in TsbPD mode, hence this simpler function
 * that only check if first packet in queue is ready.
 */
-bool CRcvBuffer::isRcvDataReady(uint64_t& tsbpdtime, CPacket** pppkt)
+bool CRcvBuffer::isRcvDataReady(ref_t<uint64_t> tsbpdtime, ref_t<int32_t> curpktseq)
 {
    tsbpdtime = 0;
 
@@ -1120,8 +1119,7 @@ bool CRcvBuffer::isRcvDataReady(uint64_t& tsbpdtime, CPacket** pppkt)
             * Only say ready if time to deliver.
             * Report the timestamp, ready or not.
             */
-            if ( pppkt )
-               *pppkt = pkt;
+            curpktseq = pkt->getSeqNo();
             tsbpdtime = getPktTsbPdTime(pkt->getMsgTimeStamp());
             if (tsbpdtime <= CTimer::getTime())
                return true;
@@ -1151,8 +1149,9 @@ CPacket* CRcvBuffer::getRcvReadyPacket()
 bool CRcvBuffer::isRcvDataReady()
 {
    uint64_t tsbpdtime;
+   int32_t seq;
 
-   return isRcvDataReady(tsbpdtime);
+   return isRcvDataReady(Ref(tsbpdtime), Ref(seq));
 }
 
 int CRcvBuffer::getAvailBufSize() const
@@ -1509,8 +1508,9 @@ int CRcvBuffer::readMsg(char* data, int len, uint64_t& tsbpdtime)
     if (m_bTsbPdMode)
     {
         passack = false;
+      int seq = 0;
 
-        if (getRcvReadyMsg(Ref(tsbpdtime)))
+      if (getRcvReadyMsg(Ref(tsbpdtime), Ref(seq)))
         {
             empty = false;
 
