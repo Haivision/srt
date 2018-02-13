b15f48c43a245d155a964f07f0fb6f10faf74d6a
lars18th <user@user.org>
2018-02-13 19:48:30 +0100

    include some info in the make script

--- examples/make-test-c.sh
+++ examples/make-test-c.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Only as example!
 gcc -I./include -L./lib test-c-client.c -lsrt -o test-c-client -lssl -lcrypto -lpthread -lstdc++
 gcc -I./include -L./lib test-c-server.c -lsrt -o test-c-server -lssl -lcrypto -lpthread -lstdc++
+
+# execute server: ./test-c-server 0.0.0.0 15012
+# execyte client: ./test-c-client 127.0.0.1 15012
+
