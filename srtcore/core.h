108bae3b4d3a8ebd73d489b92e0fc69283469dce
Sektor van Skijlen <ethouris@gmail.com>
2018-03-05 17:14:07 +0100

    Removed CBR code (#294)

--- srtcore/core.h
+++ srtcore/core.h
@@ -459,9 +459,6 @@ private: // Sending related data
    int32_t m_iLastDecSeq;                       // Sequence number sent last decrease occurs
    int32_t m_iSndLastAck2;                      // Last ACK2 sent back
    uint64_t m_ullSndLastAck2Time;               // The time when last ACK2 was sent back
-#ifdef SRT_ENABLE_CBRTIMESTAMP
-   uint64_t m_ullSndLastCbrTime;                 // Last timestamp set in a data packet to send (usec)
-#endif
 
    int32_t m_iISN;                              // Initial Sequence Number
 #ifdef SRT_ENABLE_TSBPD
