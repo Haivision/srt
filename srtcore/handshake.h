89f261fce41cf1c8596d0778ea92cb4d48ae77c9
Roman <romandi@gmail.com>
2018-04-03 14:09:15 -0400

    Change License to MPLv2.0 (#327)
    * Change License to MPLv2.0
    
    * Add original IoU license to handshake.* sources

--- srtcore/handshake.h
+++ srtcore/handshake.h
@@ -1,3 +1,13 @@
+/*
+ * SRT - Secure, Reliable, Transport
+ * Copyright (c) 2018 Haivision Systems Inc.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ */
+
 /*****************************************************************************
 Copyright (c) 2001 - 2011, The Board of Trustees of the University of Illinois.
 All rights reserved.
