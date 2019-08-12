7f5bdcbe2860296db5b0f744e3cf068edec9d112
Mikołaj Małecki <mmalecki@haivision.com>
2019-08-12 12:21:55 +0200

    Fixed correct symbols. Fixed also other occurrences where almost C++11 is suported

--- srtcore/utilities.h
+++ srtcore/utilities.h
@@ -26,24 +26,22 @@ written by
 #endif
 
 #if defined(__cplusplus) && __cplusplus > 199711L
+#define HAVE_CXX11 1
 
 // For gcc 4.7, claim C++11 is supported, as long as experimental C++0x is on,
 // however it's only the "most required C++11 support".
-#if defined(__GXX_EXPERIMENTAL_CXX0X__) && __GNUC_MAJOR__ == 4 && __GNUC_MINOR__ >= 7 // 4.7 only!
-#define HAVE_CXX11 1
+#if defined(__GXX_EXPERIMENTAL_CXX0X__) && __GNUC__ == 4 && __GNUC_MINOR__ >= 7 // 4.7 only!
 #define ATR_NOEXCEPT
 #define ATR_CONSTEXPR
 #define ATR_OVERRIDE
 #define ATR_FINAL
 #else
-#define HAVE_CXX11 1
 #define HAVE_FULL_CXX11 1
 #define ATR_NOEXCEPT noexcept
 #define ATR_CONSTEXPR constexpr
 #define ATR_OVERRIDE override
 #define ATR_FINAL final
 #endif
-
 
 // Microsoft Visual Studio supports C++11, but not fully,
 // and still did not change the value of __cplusplus. Treat
