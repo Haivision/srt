72819178bbf8b5450a475e4214abc04ccb5ba40d
Mikołaj Małecki <mmalecki@haivision.com>
2019-09-06 12:39:33 +0200

    Post-review fixes

--- srtcore/fec.h
+++ srtcore/fec.h
@@ -1,3 +1,14 @@
+/*
+ * SRT - Secure, Reliable, Transport
+ * Copyright (c) 2019 Haivision Systems Inc.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ */
+
+
 #ifndef INC__SRT_FEC_H
 #define INC__SRT_FEC_H
 
