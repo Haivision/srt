9302dad1b34d28f2464782269732c1d98744478d
Mikołaj Małecki <mmalecki@haivision.com>
2019-11-29 17:41:24 +0100

    Fixed deprecated option declarations to work in C mode

--- examples/recvlive.cpp
+++ examples/recvlive.cpp
@@ -26,7 +26,7 @@ int main(int argc, char* argv[])
    // use this function to initialize the UDT library
    srt_startup();
 
-   srt_setloglevel(logging::LogLevel::debug);
+   srt_setloglevel(srt_logging::LogLevel::debug);
 
    addrinfo hints;
    addrinfo* res;
