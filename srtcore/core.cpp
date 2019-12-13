0a27cdb6b0cf1f4e3357ac586a09d8bf5a8a58b5
Sektor van Skijlen <ethouris@gmail.com>
2019-12-13 05:01:20 +0100

    [core] Added checks around CC (#1011)

--- srtcore/core.cpp
+++ srtcore/core.cpp
@@ -5237,7 +5237,11 @@ SRT_REJECT_REASON CUDT::setupCC()
               << " rcvrate=" << m_iDeliveryRate << "p/s (" << m_iByteDeliveryRate << "B/S)"
               << " rtt=" << m_iRTT << " bw=" << m_iBandwidth);
 
-    updateCC(TEV_INIT, TEV_INIT_RESET);
+    if (!updateCC(TEV_INIT, TEV_INIT_RESET))
+    {
+        LOGC(mglog.Error, log << "setupCC: IPE: resrouces not yet initialized!");
+        return SRT_REJ_IPE;
+    }
     return SRT_REJ_UNKNOWN;
 }
 
@@ -6653,7 +6657,7 @@ void CUDT::bstats(CBytePerfMon *perf, bool clear, bool instantaneous)
     }
 }
 
-void CUDT::updateCC(ETransmissionEvent evt, EventVariant arg)
+bool CUDT::updateCC(ETransmissionEvent evt, EventVariant arg)
 {
     // Special things that must be done HERE, not in SrtCongestion,
     // because it involves the input buffer in CUDT. It would be
@@ -6667,7 +6671,7 @@ void CUDT::updateCC(ETransmissionEvent evt, EventVariant arg)
              log << "updateCC: CAN'T DO UPDATE - congctl " << (m_CongCtl.ready() ? "ready" : "NOT READY")
                  << "; sending buffer " << (m_pSndBuffer ? "NOT CREATED" : "created"));
 
-        return;
+        return false;
     }
 
     HLOGC(mglog.Debug, log << "updateCC: EVENT:" << TransmissionEventStr(evt));
@@ -6772,6 +6776,8 @@ void CUDT::updateCC(ETransmissionEvent evt, EventVariant arg)
     if (!(callcnt++ % 250)) cerr << "SndPeriod=" << (m_ullInterval_tk/m_ullCPUFrequency) << "\n");
 
 #endif
+
+    return true;
 }
 
 void CUDT::initSynch()
