f91a172e61ccb11faceae9a48a3c52c7314b792b
Maxim Sharabayko <maxlovic@gmail.com>
2020-02-24 14:17:20 +0100

    [core] Replaced CTimer with new CTimer

--- srtcore/sync.h
+++ srtcore/sync.h
@@ -551,6 +551,37 @@ private:
 };
 
 
+class CTimer
+{
+public:
+    CTimer();
+    ~CTimer();
+
+public:
+    /// Causes the current thread to block until
+    /// the specified time is reached.
+    /// Sleep can be interrupted by calling interrupt()
+    /// or woken up to recheck the scheduled time by tick()
+    /// @param tp target time to sleep until
+    ///
+    /// @return true  if the specified time was reached
+    ///         false should never happen
+    bool sleep_until(steady_clock::time_point tp);
+
+    /// Resets target wait time and interrupts waiting
+    /// in sleep_until(..)
+    void interrupt();
+
+    /// Wakes up waiting thread (sleep_until(..)) without
+    /// changing the target waiting time to force a recheck
+    /// of the current time in comparisson to the target time.
+    void tick();
+
+private:
+    CEvent m_event;
+    steady_clock::time_point m_tsSchedTime;
+};
+
 
 /// Print steady clock timepoint in a human readable way.
 /// days HH:MM::SS.us [STD]
