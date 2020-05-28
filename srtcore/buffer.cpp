1afbeaffb1228810ae03624e68e6566768253115
Mikołaj Małecki <mmalecki@haivision.com>
2020-05-28 20:11:27 +0200

    TEMPORARY added printing of readBuffer call

--- srtcore/buffer.cpp
+++ srtcore/buffer.cpp
@@ -877,6 +877,7 @@ int CRcvBuffer::addData(CUnit* unit, int offset)
 
 int CRcvBuffer::readBuffer(char* data, int len)
 {
+    std::cerr << "CRcvBuffer::readBuffer -this " << this << " -data " << data << " -len " << len << std::endl;
     int p = m_iStartPos;
     int lastack = m_iLastAckPos;
     int rs = len;
