46a8110467e7e29557f39f994a4b5239d40a5239
Lewis Kirkaldie <lewis@cinegy.com>
2020-06-11 12:40:49 +0100

    -fiddling with appveyor

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -12,14 +12,16 @@ if($Env:APPVEYOR){
 }
 $Env:VS_VERSION = $VS_VERSION
  # build SRT
-if ( $VS_VERSION -eq '2019' -and $DEVENV_PLATFORM -eq 'Win32' ) { $CMAKE_GENERATOR = 'Visual Studio 16 2019 Win32' }
-if ( $VS_VERSION -eq '2019' -and $DEVENV_PLATFORM -eq 'x64' ) { $CMAKE_GENERATOR = 'Visual Studio 16 2019' }
+if ( $VS_VERSION -eq '2019' -and $DEVENV_PLATFORM -eq 'Win32' ) { $CMAKE_GENERATOR = 'Visual Studio 16 2019'; $CMAKE_ARCH = "-A Win64" }
+if ( $VS_VERSION -eq '2019' -and $DEVENV_PLATFORM -eq 'x64' ) { $CMAKE_GENERATOR = 'Visual Studio 16 2019'; $CMAKE_ARCH = "-A x86" }
 if ( $VS_VERSION -eq '2015' -and $DEVENV_PLATFORM -eq 'Win32' ) { $CMAKE_GENERATOR = 'Visual Studio 14 2015' }
 if ( $VS_VERSION -eq '2015' -and $DEVENV_PLATFORM -eq 'x64' ) { $CMAKE_GENERATOR = 'Visual Studio 14 2015 Win64' }
 if ( $VS_VERSION -eq '2015' ) { $ENABLE_UNITTESTS = 'ON' } else { $ENABLE_UNITTESTS = 'OFF' }
 if ( $VS_VERSION -eq '2019' ) { $ENABLE_STATIC_LINK_SSL = 'false' } else { $ENABLE_STATIC_LINK_SSL = 'false' }
+Remove-Item -Path ../_build -Recurse -Force -ErrorAction SilentlyContinue
 New-Item -ItemType Directory -Path ../_build -ErrorAction SilentlyContinue
-Set-Location ../_build
-cmake ../ -G"$CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=$CONFIGURATION -DENABLE_STDCXX_SYNC=ON -DENABLE_UNITTESTS="$ENABLE_UNITTESTS" -DOPENSSL_USE_STATIC_LIBS="$ENABLE_STATIC_LINK_SSL"
+Push-Location ../_build
+cmake ../ -G"$CMAKE_GENERATOR" $CMAKE_ARCH -DCMAKE_BUILD_TYPE=$CONFIGURATION -DENABLE_STDCXX_SYNC=ON -DENABLE_UNITTESTS="$ENABLE_UNITTESTS" -DOPENSSL_USE_STATIC_LIBS="$ENABLE_STATIC_LINK_SSL"
 ../scripts/set-version-metadata.ps1
 msbuild SRT.sln /p:Configuration=$CONFIGURATION /p:Platform=$DEVENV_PLATFORM
+Pop-Location
--- scripts/set-version-metadata.ps1
+++ scripts/set-version-metadata.ps1
@@ -31,7 +31,7 @@ if($Env:APPVEYOR){
 #find C++ resource files and update file description with branch / commit details
 $FileDescriptionStringRegex = '(\bVALUE\s+\"FileDescription\"\s*\,\s*\")([^\"]*\\\")*[^\"]*(\")'
 
-Get-ChildItem -Path "./srtcore/srt_shared.rc" | ForEach-Object {
+Get-ChildItem -Path "../srtcore/srt_shared.rc" | ForEach-Object {
     $fileName = $_
     Write-Host "Processing metadata changes for file: $fileName"
 
