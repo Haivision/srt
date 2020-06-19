92abcd616c3fe13b0d0ce682043f83846d80c479
Sektor van Skijlen <ethouris@gmail.com>
2020-06-19 19:39:56 +0200

    [apps] Replaced CreateAddrInet with CreateAddr that handles IPv6 (#1360)
    * Changed CreateAddrInet to handle v4+v6
    * Added some more fixes in apps for changed sockaddr_any

--- apps/socketoptions.hpp
+++ apps/socketoptions.hpp
@@ -240,6 +240,7 @@ const SocketOption srt_options [] {
     { "kmrefreshrate", 0, SRTO_KMREFRESHRATE, SocketOption::PRE, SocketOption::INT, nullptr },
     { "kmpreannounce", 0, SRTO_KMPREANNOUNCE, SocketOption::PRE, SocketOption::INT, nullptr },
     { "enforcedencryption", 0, SRTO_ENFORCEDENCRYPTION, SocketOption::PRE, SocketOption::BOOL, nullptr },
+    { "ipv6only", 0, SRTO_IPV6ONLY, SocketOption::PRE, SocketOption::INT, nullptr },
     { "peeridletimeo", 0, SRTO_PEERIDLETIMEO, SocketOption::PRE, SocketOption::INT, nullptr },
     { "packetfilter", 0, SRTO_PACKETFILTER, SocketOption::PRE, SocketOption::STRING, nullptr },
     { "groupconnect", 0, SRTO_GROUPCONNECT, SocketOption::PRE, SocketOption::INT, nullptr},
