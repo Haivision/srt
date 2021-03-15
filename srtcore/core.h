aef0a779af3c0fed2468e3cdb55b70117aec9575
Zhao Zhili <zhilizhao@tencent.com>
2021-03-15 12:32:39 +0800

    [core] SRTO_CONNTIMEO cannot be post-restricted

--- srtcore/core.h
+++ srtcore/core.h
@@ -116,7 +116,7 @@ enum AckDataItem
 };
 const size_t ACKD_FIELD_SIZE = sizeof(int32_t);
 
-static const size_t SRT_SOCKOPT_NPOST = 13;
+static const size_t SRT_SOCKOPT_NPOST = 12;
 extern const SRT_SOCKOPT srt_post_opt_list [];
 
 enum GroupDataItem
