b5f870947c40c5591746cdf8d94c17129ab5eb69
Zhao Zhili <zhilizhao@tencent.com>
2021-04-01 16:47:03 +0800

    Remove use of CTimer::getTime() which doesn't exist now

--- srtcore/buffer.h
+++ srtcore/buffer.h
@@ -411,9 +411,9 @@ public:
    bool isRcvDataReady(time_point& w_tsbpdtime, int32_t& w_curpktseq, int32_t seqdistance);
 
 #ifdef SRT_DEBUG_TSBPD_OUTJITTER
-   void debugTraceJitter(int64_t);
+   void debugTraceJitter(time_point t);
 #else
-   void debugTraceJitter(int64_t) {}
+   void debugTraceJitter(time_point) {}
 #endif   /* SRT_DEBUG_TSBPD_OUTJITTER */
 
    bool isRcvDataReady();
