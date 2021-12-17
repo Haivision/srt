d61bb07ce3c22c48a501f9f813aa4a0bc469397b
Maxim Sharabayko <maxsharabayko@haivision.com>
2021-12-17 11:41:54 +0100

    Relocated

--- srtcore/stats.h
+++ srtcore/stats.h
@@ -126,6 +126,96 @@ struct Metric
     }
 };
 
+/// Sender-side statistics.
+struct Sender
+{
+    Metric<BytesPackets> sent;
+    Metric<BytesPackets> sentUnique;
+    Metric<BytesPackets> sentRetrans; // The number of data packets retransmitted by the sender.
+    Metric<Packets> lost; // The number of packets reported lost (including repeated reports) to the sender in NAKs.
+    Metric<BytesPackets> dropped; // The number of data packets dropped by the sender.
+
+    Metric<Packets> sentFilterExtra; // The number of packets generate by the packet filter and sent by the sender.
+    
+    Metric<Packets> recvdAck; // The number of ACK packets received by the sender.
+    Metric<Packets> recvdNak; // The number of ACK packets received by the sender.
+
+    void reset()
+    {
+        sent.reset();
+        sentUnique.reset();
+        sentRetrans.reset();
+        lost.reset();
+        dropped.reset();
+        recvdAck.reset();
+        recvdNak.reset();
+        sentFilterExtra.reset();
+    }
+
+    void resetTrace()
+    {
+        sent.resetTrace();
+        sentUnique.resetTrace();
+        sentRetrans.resetTrace();
+        lost.resetTrace();
+        dropped.resetTrace();
+        recvdAck.resetTrace();
+        recvdNak.resetTrace();
+        sentFilterExtra.resetTrace();
+    }
+};
+
+/// Receiver-side statistics.
+struct Receiver
+{
+    Metric<BytesPackets> recvd;
+    Metric<BytesPackets> recvdUnique;
+    Metric<BytesPackets> recvdRetrans; // The number of retransmitted data packets received by the receiver.
+    Metric<BytesPackets> lost; // The number of packets detected by the receiver as lost.
+    Metric<BytesPackets> dropped; // The number of packets dropped by the receiver (as too-late to be delivered).
+    Metric<BytesPackets> recvdBelated; // The number of belated packets received (dropped as too late but eventually received).
+    Metric<BytesPackets> undecrypted; // The number of packets received by the receiver that failed to be decrypted.
+
+    Metric<Packets> recvdFilterExtra; // The number of filter packets (e.g. FEC) received by the receiver.
+    Metric<Packets> suppliedByFilter; // The number of lost packets got from the packet filter at the receiver side (e.g. loss recovered by FEC).
+    Metric<Packets> lossFilter; // The number of lost DATA packets not recovered by the packet filter at the receiver side.
+
+    Metric<Packets> sentAck; // The number of ACK packets sent by the receiver.
+    Metric<Packets> sentNak; // The number of NACK packets sent by the receiver.
+
+    void reset()
+    {
+        recvd.reset();
+        recvdUnique.reset();
+        recvdRetrans.reset();
+        lost.reset();
+        dropped.reset();
+        recvdBelated.reset();
+        undecrypted.reset();
+        recvdFilterExtra.reset();
+        suppliedByFilter.reset();
+        lossFilter.reset();
+        sentAck.reset();
+        sentNak.reset();
+    }
+
+    void resetTrace()
+    {
+        recvd.resetTrace();
+        recvdUnique.resetTrace();
+        recvdRetrans.resetTrace();
+        lost.resetTrace();
+        dropped.resetTrace();
+        recvdBelated.resetTrace();
+        undecrypted.resetTrace();
+        recvdFilterExtra.resetTrace();
+        suppliedByFilter.resetTrace();
+        lossFilter.resetTrace();
+        sentAck.resetTrace();
+        sentNak.resetTrace();
+    }
+};
+
 } // namespace stats
 } // namespace srt
 
