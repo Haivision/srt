d95dafbda788c835b89455579a2a3bf47d775672
Mikołaj Małecki <mmalecki@haivision.com>
2022-11-01 16:26:38 +0100

    Refax: added size cache to the group container

--- srtcore/group.cpp
+++ srtcore/group.cpp
@@ -336,6 +336,7 @@ void CUDTGroup::GroupContainer::erase(CUDTGroup::gli_t it)
         }
     }
     m_List.erase(it);
+    --m_SizeCache;
 }
 
 void CUDTGroup::setOpt(SRT_SOCKOPT optName, const void* optval, int optlen)
