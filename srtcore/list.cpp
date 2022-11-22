97bc8aa3508f91afb0ee18feae565be7387a7569
Mikołaj Małecki <ethouris@gmail.com>
2022-11-22 15:46:06 +0100

    Blocked unnecessary debug logs

--- srtcore/list.cpp
+++ srtcore/list.cpp
@@ -735,13 +735,13 @@ int32_t srt::CRcvLossList::removeUpTo(int32_t seqno_last)
     int32_t first = getFirstLostSeq();
     if (first == SRT_SEQNO_NONE)
     {
-        HLOGC(tslog.Debug, log << "rcv-loss: DROP to %" << seqno_last << " - empty list");
+        //HLOGC(tslog.Debug, log << "rcv-loss: DROP to %" << seqno_last << " - empty list");
         return first; // empty, so nothing to remove
     }
 
     if (CSeqNo::seqcmp(seqno_last, first) < 0)
     {
-        HLOGC(tslog.Debug, log << "rcv-loss: DROP to %" << seqno_last << " - first %" << first << " is newer, exitting");
+        //HLOGC(tslog.Debug, log << "rcv-loss: DROP to %" << seqno_last << " - first %" << first << " is newer, exitting");
         return first; // seqno_last older than first - nothing to remove
     }
 
@@ -751,7 +751,7 @@ int32_t srt::CRcvLossList::removeUpTo(int32_t seqno_last)
     // that are earlier than this.
     for (int32_t i = first; CSeqNo::seqcmp(i, seqno_last) <= 0; i = CSeqNo::incseq(i))
     {
-        HLOGC(tslog.Debug, log << "... removing %" << i);
+        //HLOGC(tslog.Debug, log << "... removing %" << i);
         remove(i);
     }
 
