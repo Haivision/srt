366ac7868fdf9a97d178b42961dfe52b6e10d1d7
Mikołaj Małecki <mmalecki@haivision.com>
2022-12-02 12:30:15 +0100

    Forcing crash in test-srt to test the crash-tracking on Travis

--- test/test_common.cpp
+++ test/test_common.cpp
@@ -16,6 +16,11 @@ void test_cipaddress_pton(const char* peer_ip, int family, const uint32_t (&ip)[
     ss.ss_family = family;
 
     void* sin_addr = nullptr;
+
+    // FORCE CRASH
+    ((char*)sin_addr)[0] = 0;
+
+
     if (family == AF_INET)
     {
         sockaddr_in* const sa = (sockaddr_in*)&ss;
