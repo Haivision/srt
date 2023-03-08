a126316c729fc6863fdd6df52514f89792ffaf03
Mikołaj Małecki <mmalecki@haivision.com>
2023-03-08 15:07:43 +0100

    Added SRT test setup support

--- test/test_env.h
+++ test/test_env.h
@@ -1,18 +1,23 @@
+#ifndef INC_SRT_TESTENV_H
+#define INC_SRT_TESTENV_H
+
 #include <string>
 #include <vector>
 #include <map>
 #include <stdexcept>
 #include "gtest/gtest.h"
 
-class SrtTestEnv: public testing::Environment
+
+namespace srt
+{
+class TestEnv: public testing::Environment
 {
 public:
-    static SrtTestEnv* me;
-
+    static TestEnv* me;
     std::vector<std::string> args;
     std::map<std::string, std::vector<std::string>> argmap;
 
-    explicit SrtTestEnv(int argc, char** argv)
+    explicit TestEnv(int argc, char** argv)
         : args(argv+1, argv+argc)
     {
         if (me)
@@ -30,4 +35,54 @@ public:
     }
 
     std::string OptionValue(const std::string& key);
+
+    // Specific test environment options
+    // All must be static, return bool. Arguments allowed.
+    // The name must start with Allowed_.
+    static bool Allowed_IPv6();
 };
+
+#define SRTST_REQUIRES(feature,...) if (!srt::TestEnv::Allowed_##feature(__VA_ARGS__)) { return; }
+
+
+class TestInit
+{
+public:
+    int ninst;
+
+    static void start(int& w_retstatus);
+    static void stop();
+
+    TestInit() { start((ninst)); }
+    ~TestInit() { stop(); }
+
+    void HandleOptions();
+
+};
+
+class Test: public testing::Test
+{
+    std::unique_ptr<TestInit> init_holder;
+public:
+
+    virtual void setup() = 0;
+    virtual void teardown() = 0;
+
+    void SetUp() override final
+    {
+        init_holder.reset(new TestInit);
+        setup();
+    }
+
+    void TearDown() override final
+    {
+        teardown();
+        init_holder.reset();
+    }
+};
+
+struct sockaddr_any CreateAddr(const std::string& name, unsigned short port, int pref_family);
+
+} //namespace
+
+#endif
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -6,19 +6,25 @@
 #include "gtest/gtest.h"
 #include "test_env.h"
 
-using namespace std;
+#include "srt.h"
+#include "netinet_any.h"
 
-SrtTestEnv* SrtTestEnv::me = 0;
+using namespace std;
 
 int main(int argc, char **argv)
 {
     string command_line_arg(argc == 2 ? argv[1] : "");
     testing::InitGoogleTest(&argc, argv);
-    testing::AddGlobalTestEnvironment(new SrtTestEnv(argc, argv));
+    testing::AddGlobalTestEnvironment(new srt::TestEnv(argc, argv));
     return RUN_ALL_TESTS();
 }
 
-void SrtTestEnv::FillArgMap()
+namespace srt
+{
+
+TestEnv* TestEnv::me = 0;
+
+void TestEnv::FillArgMap()
 {
     // The rule is:
     // - first arguments go to an empty string key
@@ -28,6 +34,8 @@ void SrtTestEnv::FillArgMap()
 
     string key;
     bool expectkey = true;
+
+    argmap[""];
 
     for (auto& a: args)
     {
@@ -40,7 +48,10 @@ void SrtTestEnv::FillArgMap()
                 else if (a[1] == '/')
                     key = "";
                 else
+                {
                     key = a.substr(1);
+                    argmap[key]; // Make sure it exists even empty
+                }
 
                 continue;
             }
@@ -51,8 +62,7 @@ void SrtTestEnv::FillArgMap()
     return;
 }
 
-
-std::string SrtTestEnv::OptionValue(const std::string& key)
+std::string TestEnv::OptionValue(const std::string& key)
 {
     std::ostringstream out;
 
@@ -68,4 +78,103 @@ std::string SrtTestEnv::OptionValue(const std::string& key)
     }
 
     return out.str();
+}
+
+// Specific functions
+bool TestEnv::Allowed_IPv6()
+{
+    if (TestEnv::me->OptionPresent("disable-ipv6"))
+    {
+        std::cout << "TEST: IPv6 testing disabled, FORCED PASS\n";
+        return false;
+    }
+    return true;
+}
+
+
+void TestInit::start(int& w_retstatus)
+{
+    ASSERT_GE(w_retstatus = srt_startup(), 0);
+}
+
+void TestInit::stop()
+{
+    EXPECT_NE(srt_cleanup(), -1);
+}
+
+// This function finds some interesting options among command
+// line arguments and does specific things.
+void TestInit::HandleOptions()
+{
+    // As a short example:
+    // use '-logdebug' option to turn on debug logging.
+
+    if (TestEnv::me->OptionPresent("logdebug"))
+    {
+        srt_setloglevel(LOG_DEBUG);
+    }
+}
+
+// Copied from ../apps/apputil.cpp, can't really link this file here.
+sockaddr_any CreateAddr(const std::string& name, unsigned short port, int pref_family)
+{
+    using namespace std;
+
+    // Handle empty name.
+    // If family is specified, empty string resolves to ANY of that family.
+    // If not, it resolves to IPv4 ANY (to specify IPv6 any, use [::]).
+    if (name == "")
+    {
+        sockaddr_any result(pref_family == AF_INET6 ? pref_family : AF_INET);
+        result.hport(port);
+        return result;
+    }
+
+    bool first6 = pref_family != AF_INET;
+    int families[2] = {AF_INET6, AF_INET};
+    if (!first6)
+    {
+        families[0] = AF_INET;
+        families[1] = AF_INET6;
+    }
+
+    for (int i = 0; i < 2; ++i)
+    {
+        int family = families[i];
+        sockaddr_any result (family);
+
+        // Try to resolve the name by pton first
+        if (inet_pton(family, name.c_str(), result.get_addr()) == 1)
+        {
+            result.hport(port); // same addr location in ipv4 and ipv6
+            return result;
+        }
+    }
+
+    // If not, try to resolve by getaddrinfo
+    // This time, use the exact value of pref_family
+
+    sockaddr_any result;
+    addrinfo fo = {
+        0,
+        pref_family,
+        0, 0,
+        0, 0,
+        NULL, NULL
+    };
+
+    addrinfo* val = nullptr;
+    int erc = getaddrinfo(name.c_str(), nullptr, &fo, &val);
+    if (erc == 0)
+    {
+        result.set(val->ai_addr);
+        result.len = result.size();
+        result.hport(port); // same addr location in ipv4 and ipv6
+    }
+    freeaddrinfo(val);
+
+    return result;
+}
+
+
 }
